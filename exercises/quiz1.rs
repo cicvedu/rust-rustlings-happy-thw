62bda41c01d9edf7cf90065d07c4d9c65b280934
happy-thw <tangq7443@163.com>
2023-08-12 19:11:23 +0800

    finish 50/100

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,13 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(Unit_price:i32) -> i32 {
+    if Unit_price <= 40 {
+        Unit_price*2 
+    } else {Unit_price}
+    }
 
 // Don't modify this function!
 #[test]
