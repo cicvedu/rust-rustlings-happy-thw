784324f12ca35917ac22c9b75470b387a9b773c0
TangHW <69003433@gitlab.com>
2023-08-14 17:22:36 +0800

    anything

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -29,21 +29,23 @@ impl Default for Person {
 // age component into a `usize` with something like `"4".parse::<usize>()`. The
 // outcome of this needs to be handled appropriately.
 //
-// Steps:
-// 1. If the length of the provided string is 0, then return the default of
-//    Person.
-// 2. Split the given string on the commas present in it.
-// 3. Extract the first element from the split operation and use it as the name.
-// 4. If the name is empty, then return the default of Person.
-// 5. Extract the other element from the split operation and parse it into a
-//    `usize` as the age.
-// If while parsing the age, something goes wrong, then return the default of
-// Person Otherwise, then return an instantiated Person object with the results
+
 
 // I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+    // Steps:
+    // 1. If the length of the provided string is 0, then return the default of
+    //    Person.
+    // 2. Split the given string on the commas present in it.
+    // 3. Extract the first element from the split operation and use it as the name.
+    // 4. If the name is empty, then return the default of Person.
+    // 5. Extract the other element from the split operation and parse it into a
+    //    `usize` as the age.
+    // If while parsing the age, something goes wrong, then return the default of
+    // Person Otherwise, then return an instantiated Person object with the results
+      s.split_one()
     }
 }
 
